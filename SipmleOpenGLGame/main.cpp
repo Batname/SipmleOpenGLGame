21616c826ff0c907fd5a1307f61d17927725932b
DenisDubinin <dadubinin@gmail.com>
2016-11-14 22:24:14 +0100

    Initial Commit

---
+++ SipmleOpenGLGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SipmleOpenGLGame
+//
+//  Created by apple on 2016-11-14.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
