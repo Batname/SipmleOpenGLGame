5404de367d1dce7a280995a8698a0af8185670ab
DenisDubinin <dadubinin@gmail.com>
2016-11-15 17:13:39 +0100

    add glfw lib

--- SipmleOpenGLGame/main.cpp
+++ SipmleOpenGLGame/main.cpp
@@ -1,11 +1,19 @@
-#include <iostream>
-//#include <GL/glew.h>
-//#include <GLFW/glfw3.h>
-
 #include <GL/glfw.h>
 
 int main(int argc, const char * argv[]) {
+    bool running = true;
     glfwInit();
+    glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
+    glfwOpenWindow(800*16/9, 800, 8, 8, 8, 8, 0, 0, GLFW_WINDOW);
+    glfwSetWindowTitle("OpenGL title");
+    
+    while(running) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers();
 
+        running = glfwGetWindowParam(GLFW_OPENED);
+    }
+    
+    glfwTerminate();
     return 0;
 }
