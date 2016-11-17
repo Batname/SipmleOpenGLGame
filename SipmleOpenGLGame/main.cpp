74434bc8a76d4e2085c546ff69542542db730c3a
DenisDubinin <dadubinin@gmail.com>
2016-11-17 08:48:22 +0100

    update each 60 sec

--- SipmleOpenGLGame/main.cpp
+++ SipmleOpenGLGame/main.cpp
@@ -2,6 +2,8 @@
 
 #include <GL/glfw.h>
 
+#define Updates_Per_Second 60
+
 GameWindow * gameWindow;
 
 int main(int argc, const char * argv[]) {
@@ -9,13 +11,24 @@ int main(int argc, const char * argv[]) {
     glfwInit();
     glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
     glfwOpenWindow(800*16/9, 800, 8, 8, 8, 8, 0, 0, GLFW_WINDOW);
-    glfwSetWindowTitle("OpenGL title");
+    glfwSetWindowTitle("OpenGL Game");
+    glfwSwapInterval(0);
     
     gameWindow = new GameWindow(true);
     
+    double lastTime = glfwGetTime();
+    double deltaTime = 0.0f;
+    
     while(gameWindow->getRunning()) {
         gameWindow->render();
-        gameWindow->update();
+        
+        deltaTime += (glfwGetTime() - lastTime) * Updates_Per_Second;
+        lastTime = glfwGetTime();
+        while (deltaTime >= 1.0f) {
+            gameWindow->update();
+            --deltaTime;
+        }
+
         gameWindow->setRuning(glfwGetWindowParam(GLFW_OPENED));
     }
     
